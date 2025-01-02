73c92825f868d38d4bf544ca2dd4c5bd1503be0b
SKolokythas <kolokythas61@gmail.com>
2025-01-02 13:03:46 +0200

    Αρχικά αρχεία για έργο MakeCode

---
+++ main.ts
@@ -0,0 +1,4 @@
+basic.showIcon(IconNames.Heart)
+TPBot.setServo(TPBot.ServoTypeList.S360, TPBot.ServoList.S1, 120)
+TPBot.setTravelTime(TPBot.DriveDirection.Forward, 30, 2)
+TPBot.setServo(TPBot.ServoTypeList.S360, TPBot.ServoList.S1, 180)
